2bccd133058c75fce69ee555f9f5c2f10bf9ccfe
lihaibin <lihaibin928383374@gmail.com>
2021-04-13 13:05:17 +0800

    新增服务：mall-third-part，用于管理第三方服务

---
+++ mall-third-part/src/main/java/com/example/mallthirdpart/MallThirdPartApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallthirdpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallThirdPartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallThirdPartApplication.class, args);
+    }
+
+}
