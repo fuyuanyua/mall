fc7fa7e177d8db0db09102996a6a16fd7c812154
lihaibin <lihaibin928383374@gmail.com>
2021-04-24 23:10:19 +0800

    mall-search微服务：测试索引一个数据到es

--- mall-search/src/test/java/com/example/mallsearch/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/example/mallsearch/MallSearchApplicationTests.java
@@ -1,13 +1,56 @@
 package com.example.mallsearch;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+
+@Slf4j
 @SpringBootTest
 class MallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
 
     @Test
     void contextLoads() {
     }
+
+    /**
+     * 测试存储数据到es
+     */
+    @Test
+    public void indexData() throws IOException {
+        User user = new User();
+        user.setUsername("小明").setAge(18).setGender(2);
+        String jsonString = JSON.toJSONString(user);
+        IndexRequest indexRequest = new IndexRequest("user");
+        indexRequest.source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+        log.info("{}", indexResponse);
+    }
+
+    @Data
+    @Accessors(chain = true)
+    class User {
+        private String username;
+
+        private Integer age;
+
+        private Integer gender;
+    }
+
+
 
 }
