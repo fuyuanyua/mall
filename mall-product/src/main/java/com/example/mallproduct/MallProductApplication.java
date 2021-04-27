8d71b832d002af1039dfc8c450fe3c0fce5a3cdc
lihaibin <lihaibin928383374@gmail.com>
2021-04-27 23:26:41 +0800

    mall-product微服务：测试redis

--- mall-product/src/main/java/com/example/mallproduct/MallProductApplication.java
+++ mall-product/src/main/java/com/example/mallproduct/MallProductApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 1.整合redis
+ * 1.整合Redis
  *      1.)引入spring-boot-starter-data-redis依赖
  *      2.)配置文件中配置host、port、username、password等
- *      3.)使用SpringBoot根据配置文件自动帮我们配置好并纳入容器的RedisTemplate实例来操作redis
+ *      3.)使用SpringBoot根据配置文件自动帮我们配置好并纳入容器的RedisTemplate实例来操作Redis
+ *          3.1)找自动配置类的方法：一般都是以xxxAutoConfiguration命名
+ *      4.)无论是Lettuce还是Jedis都是操作Redis的底层客户端，RedisTemplate是SpringBoot对底层客户端的再次封装
  */
 
 @MapperScan("com.example.mallproduct.dao")
--- mall-product/src/main/java/com/example/mallproduct/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/example/mallproduct/service/impl/BrandServiceImpl.java
@@ -1,5 +1,12 @@
 package com.example.mallproduct.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,13 +18,40 @@ import com.example.common.utils.Query;
 import com.example.mallproduct.dao.BrandDao;
 import com.example.mallproduct.entity.BrandEntity;
 import com.example.mallproduct.service.BrandService;
+import org.springframework.util.CollectionUtils;
 
 
 @Service("brandService")
 public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> implements BrandService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        // 如果查全量数据，走缓存
+        if (CollectionUtils.isEmpty(params)) {
+            ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+            // 查缓存
+            String all = operations.get("all");
+            // 缓存中未查到
+            if (StringUtils.isEmpty(all)) {
+                // 查库
+                PageUtils pageUtils = queryPageFromDB(params);
+                String jsonString = JSON.toJSONString(pageUtils);
+                // 保存数据到redis
+                operations.set("all", jsonString);
+                return pageUtils;
+            }
+            // 缓存中查到
+            PageUtils pageUtils = JSON.parseObject(all, PageUtils.class);
+            return pageUtils;
+        }
+        // 查分页数据走DB
+        return queryPageFromDB(params);
+    }
+
+    public PageUtils queryPageFromDB(Map<String, Object> params) {
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
                 new QueryWrapper<BrandEntity>()
